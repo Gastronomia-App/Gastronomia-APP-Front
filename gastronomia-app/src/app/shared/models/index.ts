83d9126609b6df314f38350f3aeeebde5fc0b653
Pierx <pierofelixvisitacionromero@gmail.com>
2025-10-26 13:39:42 -0300

    Add customer model, images, and new styles
    Introduces Customer and CustomerFilter interfaces and exports them. Adds several UI images for customer-related actions. Implements new page and table CSS styles, including pagination and button enhancements. Updates variables.css with new color and shadow variables for improved UI consistency.

--- gastronomia-app/src/app/shared/models/index.ts
+++ gastronomia-app/src/app/shared/models/index.ts
@@ -1,2 +1,3 @@
 export type { Product, ProductComponent, ProductGroup } from './product.model';
 export type { Category } from './category.model';
+export type { Customer, CustomerFilter} from './customer.model';
