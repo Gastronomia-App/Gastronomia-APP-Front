918053e51c69690772ab28b335d326ff023c1e0b
MaximoAndriola <maximoandriola2017@gmail.com>
2025-10-27 16:44:48 -0300

    Add main application routes
    Defined routes for home, expenses, suppliers, products, customers, and employees pages. Added default and wildcard redirects to the home page.

--- gastronomia-app/src/app/app.routes.ts
+++ gastronomia-app/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./domains/home/home-page').then(m => m.HomePage)
+  },
+  {
+    path: 'expenses',
+    loadComponent: () => import('./domains/expenses/expenses-page').then(m => m.ExpensesPage)
+  },
+  {
+    path: 'suppliers',
+    loadComponent: () => import('./domains/suppliers/suppliers-page').then(m => m.SuppliersPage)
+  },
+  {
+    path: 'products',
+    loadComponent: () => import('./domains/products/products-page').then(m => m.ProductsPage)
+  },
+  {
+    path: 'customers',
+    loadComponent: () => import('./domains/customers/customers-page').then(m => m.CustomersPage)
+  },
+  {
+    path: 'employees',
+    loadComponent: () => import('./domains/employees/employees-page').then(m => m.EmployeesPage)
+  },
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
+];
