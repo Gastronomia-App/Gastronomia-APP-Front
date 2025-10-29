c78757cf09cb4a1aa23759bec3d61d856f5b0548
Pierx <pierofelixvisitacionromero@gmail.com>
2025-10-29 15:13:59 -0300

    Local progress before merging with shared and dev

--- gastronomia-app/src/app/app.ts
+++ gastronomia-app/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CustomerPage } from './domains/customer/pages/customer-page/customer-page';
+import { Header } from './shared/components/header/header';
+import { Footer } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CustomerPage, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- gastronomia-app/src/app/shared/models/index.ts
+++ gastronomia-app/src/app/shared/models/index.ts
@@ -2,5 +2,4 @@ export type { Product, ProductComponent, ProductGroup } from './product.model';
 export type { Category } from './category.model';
 export type { Customer, CustomerFilter} from './customer.model';
 export type { PageResponse } from './pageable.model';
-export type { Supplier } from './supplier.model';
 export type { Expense } from './expense.model';
