7c54b21fb00beca0ddc8a2239c29495554d33940
MaximoAndriola <maximoandriola2017@gmail.com>
2025-11-10 19:56:30 -0300

    Add OrderService for managing orders via API
    Introduces OrderService with methods to fetch, filter, retrieve by ID, and delete orders using HTTP requests. Supports pagination, sorting, and various filter options for order retrieval.

--- gastronomia-app/src/app/shared/models/order.model.ts
+++ gastronomia-app/src/app/shared/models/order.model.ts
@@ -1,24 +1,19 @@
-import { Employee } from './employee.model';
-import { Customer } from './customer.model';
-import { Business } from './business.model';
 import { Item } from './item.model';
-import { Seating } from './seating';
 
 export type OrderType = 'TABLE' | 'TAKEAWAY' | 'DELIVERY';
 export type OrderStatus = 'FINALIZED' | 'IN_PROGRESS' | 'CANCELED';
 
 export interface Order {
   id: number;
-  employee?: Employee;
-  customer?: Customer;
-  seating?: Seating;
+  employeeName?: string;
+  customerName?: string;
+  seatingNumber?: number;
+  orderType: OrderType;
   items: Item[];
-  type: OrderType;
   dateTime: string;
-  discount: number;
   peopleCount?: number;
-  status: OrderStatus;
+  discount: number;
+  status: string;
   subtotal: number;
   total: number;
-  business?: Business;
 }
